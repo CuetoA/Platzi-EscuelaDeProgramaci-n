bf8011cc8533e2f2fe522b29fc090d84c3e82fc4
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2021-12-28 00:50:04 -0600

    segundo valor mayor js

---
+++ 0 Algoritmos y pensamiento lógico/1.4-segundoValor.js
@@ -0,0 +1,10 @@
+var arr = [10,2,1,3,5,7,6,9,8];
+
+function segundoMayorNumero(arreglo){
+  let len = arreglo.length;
+  console.log('arreglo es: ', len)
+  //for ()
+};
+
+console.log('probando');
+segundoMayorNumero(arr);
