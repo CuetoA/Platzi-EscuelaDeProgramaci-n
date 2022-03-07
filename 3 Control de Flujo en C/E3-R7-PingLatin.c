279e2b2c51b3d89ea39f64321b8f6029dbdd3483
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-06 22:21:23 -0600

    Finished E3-R7-PingLatin and exercise E3

---
+++ 3 Control de Flujo en C/E3-R7-PingLatin.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+char word[20];
+int vowel, len, newlen;
+
+
+
+void vowelCase(){
+    printf("test\n");
+    len = strlen(word);
+    newlen = len + 3;
+
+    strcat(word, "way");
+    printf("salida: %s", word);
+
+}
+
+
+void consonantCase(){
+    char temp = word[0];
+    int i, len = strlen(word);
+
+    for(i=1; i<=len; i++){
+        //printf("\ntest: %d\n", i);
+        word[i-1] = word[i];
+    }
+    word[len-1] = temp;    
+
+    strcat(word, "ay");
+    printf("salida: %s", word);
+}
+
+
+int main(){
+
+    printf("Enter a word:");
+    gets(word);
+
+    char c = tolower(word[0]);
+    vowel = (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+
+    if(vowel){
+        vowelCase();
+    }
+    else
+        consonantCase();
+    return 0;
+}
