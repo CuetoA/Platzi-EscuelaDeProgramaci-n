11ed80fe2fb20b0dc6ddf6a3c0c7b1d11cc37d46
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-07 21:10:11 -0600

    Learned to declare a function

---
+++ 4 Funciones en C/1-FirstFunction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void hello(char* message);
+
+
+int main(){
+
+    char message[10] = "Bola";
+    hello(message);
+    return 0;
+}
+
+void hello(char* message){
+    printf("The message is: %s", message);
+}
