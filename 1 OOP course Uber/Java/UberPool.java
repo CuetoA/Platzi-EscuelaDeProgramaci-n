bebd05d6be1db70c6d0d73e4960aa05d319f6f24
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-12 00:57:25 -0600

    Added childs in Java

---
+++ 1 OOP course Uber/Java/UberPool.java
@@ -0,0 +1,12 @@
+public class UberPool {
+    String brand;
+    String model;
+
+    public UberPool(String license, Account driver, String brand, String model){
+        super(license, driver); // This represents the super class constructor
+        this.brand = brand;
+        this.model = model;
+
+        
+    }
+}
