11cd58938728bde45d0c223ab2ae42d5ef6cd4ab
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-12 22:16:25 -0600

    learned encapsulation

--- 1 OOP course Uber/Java/Car.java
+++ 1 OOP course Uber/Java/Car.java
@@ -1,13 +1,27 @@
 public class Car {
-    Integer id;
-    String license;
-    Account driver;
-    Integer passenger;
+    private Integer id;
+    private String license;
+    private Account driver;
+    private Integer passenger;
 
 
     public Car (String license, Account driver){
         this.license = license;
         this.driver = driver;
+        this.passenger = 2;
+    }
+
+    public Integer getPassenger(){
+        return passenger;
+    }
+
+    public void setPassenger(Integer passenger){
+        if(passenger ==4 ){
+            this.passenger = passenger;
+        }else{
+            System.out.println("You need to assign 4 passengers");
+        }
+        
     }
 
     void printDataCar(){
@@ -16,5 +30,31 @@ public class Car {
         System.out.println("Passenger: " + passenger);
         System.out.println();
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public Account getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Account driver) {
+        this.driver = driver;
+    }
+
+    
     
 }
--- 1 OOP course Uber/Java/main.java
+++ 1 OOP course Uber/Java/main.java
@@ -2,12 +2,13 @@ class Main{
 
     public static void main(String[] args) {
         
-        Car car = new Car("AMQ123", new Account("Andres Cueto", "AND123"));
+        UberX car = new UberX("AMQ123", new Account("Andres Cueto", "AND123"), "Suzuki", "Swift");
         car.license = "AMQ123";
+        car.setPassenger(6);
         car.printDataCar();
 
-        Car car2 = new Car("YMI270", new Account("Scarlette Bello", "SCR123"));
-        car2.printDataCar();
+        // Car car2 = new Car("YMI270", new Account("Scarlette Bello", "SCR123"));
+        // car2.printDataCar();
     }
 
 }
