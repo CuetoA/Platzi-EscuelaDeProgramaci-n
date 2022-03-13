b88a7f9de9f2744edd139382fd5f06bd61edcd19
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-12 20:21:29 -0600

    Created child classes in python

--- 1 OOP course Uber/Python/main.py
+++ 1 OOP course Uber/Python/main.py
@@ -1,5 +1,6 @@
 from car     import Car
 from account import Account
+from uberBlack import UberBlack
 
 if __name__ == "__main__":
     print("Hola mundo")
@@ -10,3 +11,6 @@ if __name__ == "__main__":
     
     car2 = Car("NPR069", Account("Alejandro Velez" , "INE") )
     car2.dataCar()
+    
+    car3 = UberBlack("NPM5576", Account("Scarlette Bello", "kjno"), ["hola"], ["adios"] )
+    car3.dataCar()
