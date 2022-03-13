b2e020058ae2d8d5d1d3b0ce7ce03ed819552d7b
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-12 20:34:49 -0600

    created and used child classes in js

--- 1 OOP course Uber/Js/index.js
+++ 1 OOP course Uber/Js/index.js
@@ -2,3 +2,7 @@ var account = new Account("Andrés Cueto", "INE");
 var car = new Car("AW324", account);
 car.passenger =4;
 car.printDataCar();
+
+var car2 = new UberVan("AW324", new Account("Scarlette Bello Barrón", "INE"), "hola", "hola2");
+car2.passenger = 5;
+car2.printDataCar();
