d1c0bb01628285370d29c80822cb369fca29e74f
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-03-12 20:42:17 -0600

    Added account childs in js

--- 1 OOP course Uber/Js/index.js
+++ 1 OOP course Uber/Js/index.js
@@ -3,6 +3,6 @@ var car = new Car("AW324", account);
 car.passenger =4;
 car.printDataCar();
 
-var car2 = new UberVan("AW324", new Account("Scarlette Bello Barrón", "INE"), "hola", "hola2");
+var car2 = new UberVan("AW324", new Driver("Scarlette Bello Barrón", "INE"), "hola", "hola2");
 car2.passenger = 5;
 car2.printDataCar();
