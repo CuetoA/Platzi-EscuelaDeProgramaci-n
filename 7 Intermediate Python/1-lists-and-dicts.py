f9b7168d3017fe6223e81624b9e1b9218c29c3b7
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-20 17:18:24 -0500

    practiced

---
+++ 7 Intermediate Python/1-lists-and-dicts.py
@@ -0,0 +1,20 @@
+
+
+def run():
+    my_list = [1, "HELLO", True, 4.5]
+    my_dict = {'fist name':'andres', 'last name':'cueto'}
+    
+    super_list = [
+        {'fist name':'andres', 'last name':'cueto'},
+        {'fist name':'scarlette', 'last name':'bello'},
+        {'fist name':'ceres', 'last name':'estrada'}
+    ]
+
+    super_dict = {
+        "numbers": [1,2,3,4,5],
+        "names": ['andres', 'scar']
+    }
+
+
+if __name__ == "__main__":
+    run()
