a89274807bd2ded89c53e961cb026a16529b4162
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-21 09:12:01 -0500

    added to readme

---
+++ 9 Frontend development/readmi.m
@@ -0,0 +1,8 @@
+In this course we are learning about how to HTML and CSS works. 
+Some resources:
+
+
+- [HTML reference](https://htmlreference.io/)
+- [Colors in HTML](https://htmlcolorcodes.com/es/)
+- [Resources for web developers](https://developer.mozilla.org/es/)
+- [Specificity calculator](https://specificity.keegan.st/)
