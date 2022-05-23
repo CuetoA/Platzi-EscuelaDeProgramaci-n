b81b0ecd7be9e0e9a10bb1c080249735222fa39e
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-22 21:29:30 -0500

    learned about most used css properties

--- 9 Frontend development/readmi.m
+++ 9 Frontend development/readmi.m
@@ -3,6 +3,7 @@ Some resources:
 
 
 - [HTML reference](https://htmlreference.io/)
+- [CSS documentation](https://cssreference.io/)
 - [Colors in HTML](https://htmlcolorcodes.com/es/)
 - [Resources for web developers](https://developer.mozilla.org/es/)
 - [Specificity calculator](https://specificity.keegan.st/)
