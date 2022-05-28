4ad48a7c1feb6d4e9903dae8d3d719fc2ee6dd33
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-05-28 14:52:49 -0500

    learning to use and operate sets

---
+++ 8 Profesional Python/6.1-sets.py
@@ -0,0 +1,17 @@
+my_set1 = {3,4,5}
+my_set2 = {5,6,7}
+
+my_set_3 = my_set1 | my_set2
+print(my_set_3)
+
+my_set_3 = my_set1 & my_set2
+print(my_set_3)
+
+my_set_3 = my_set1 - my_set2
+print(my_set_3)
+
+my_set_3 = my_set2 - my_set1
+print(my_set_3)
+
+my_set_3 = my_set1 ^ my_set2
+print(my_set_3)
