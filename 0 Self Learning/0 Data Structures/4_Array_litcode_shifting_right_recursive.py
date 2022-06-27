4647cf10bedcdf858d9c27d52dffb9ce64e5cfb8
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-06-26 19:47:37 -0500

    applied shift but something is wrong

--- 0 Self Learning/0 Data Structures/4_Array_litcode_shifting_right_recursive.py
+++ 0 Self Learning/0 Data Structures/4_Array_litcode_shifting_right_recursive.py
@@ -1,20 +1,35 @@
 
-def duplicateZeros(arr, s=0):
+def duplicateZeros(arr, s=0, end_p = 1):
 
-    if len(arr) == 1 or s>= len(arr): return arr
+    if len(arr) == 1 or s>= len(arr): return arr, len(arr)
 
     print(f's={s} \t arr={arr}')
     i = 0
     while i <= (len(arr)-1) :
         if arr[i] == 0:
             s += 1         
-            duplicateZeros(arr[i+1:], s)
+            arr[i+1:], end_p = duplicateZeros(arr[i+1:], s, end_p)
+            print(f's={s} \t arr={arr}')
             break
         else:
             i += 1
+    
+        
+
+    for j in range(len(arr)-end_p, s-1, -1):
+        arr[j] = arr[j-s]
+    arr[s-1] = 0
+    end_p = len(arr) - s -1
+
+    print(f's={s} \t arr={arr}')
+    return arr, end_p
+    
+    
+
+
 
 
 
 if __name__=="__main__":
-    arri = [0,4,0,6,3,0,0,2,5]
-    duplicateZeros(arri)
+    arri = [0,4,0,6,3,0,2,5,4,6,7]
+    print(f'\nresultado: {duplicateZeros(arri)}')
