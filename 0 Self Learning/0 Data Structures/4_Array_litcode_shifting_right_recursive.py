cc56252005e4886b4059e676ae13cf6c094b3da1
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-06-27 21:24:15 -0500

    found some cases where not

--- 0 Self Learning/0 Data Structures/4_Array_litcode_shifting_right_recursive.py
+++ 0 Self Learning/0 Data Structures/4_Array_litcode_shifting_right_recursive.py
@@ -3,13 +3,13 @@ def duplicateZeros(arr, s=0, end_p = 1):
 
     if len(arr) == 1 or s>= len(arr): return arr, len(arr)
 
-    print(f's={s} \t arr={arr}')
+    print(f's={s} \t     arr={arr}')
     i = 0
-    while i <= (len(arr)-1) :
+    while i <= (len(arr)-2) :
         if arr[i] == 0:
             #s += 1         
             arr[i+1:], end_p = duplicateZeros(arr[i+1:], s + 1, end_p)
-            print(f's={s} \t arr={arr}')
+            print(f's={s} \t     arr={arr}')
             break
         else:
             i += 1
@@ -34,7 +34,7 @@ def duplicateZeros(arr, s=0, end_p = 1):
     # arr[s] = 0
     # end_p = len(arr) - s -1
 
-    print(f's={s} \t arr={arr}')
+    # print(f's={s} \t arr={arr}')
     return arr, end_p
     
     
@@ -46,6 +46,8 @@ def duplicateZeros(arr, s=0, end_p = 1):
 if __name__=="__main__":
     arr1 = [0,4,0,6,3,0,2,5,4,6,7]
     arr2 = [1,2,3,0,4,5,6]
-    arr3 = [1,2,3,0,4,5,6,0,7,8,9]
+    arr3 = [1,2,3,0,4,5,6,0,7,8,9,0,9,10,11,13,14,15]
 
-    print(f'\nresultado: {duplicateZeros(arr3)}')
+    arr = arr1
+    print(f'input: \t\t {arr}')
+    print(f'\nresultado: \t{duplicateZeros(arr)}')
