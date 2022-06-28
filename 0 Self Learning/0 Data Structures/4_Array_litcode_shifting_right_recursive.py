62bccb4d2698f1536c68277142fc4a2290d2c396
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-06-27 21:14:11 -0500

    maybe it is finished

--- 0 Self Learning/0 Data Structures/4_Array_litcode_shifting_right_recursive.py
+++ 0 Self Learning/0 Data Structures/4_Array_litcode_shifting_right_recursive.py
@@ -7,19 +7,32 @@ def duplicateZeros(arr, s=0, end_p = 1):
     i = 0
     while i <= (len(arr)-1) :
         if arr[i] == 0:
-            s += 1         
-            arr[i+1:], end_p = duplicateZeros(arr[i+1:], s, end_p)
+            #s += 1         
+            arr[i+1:], end_p = duplicateZeros(arr[i+1:], s + 1, end_p)
             print(f's={s} \t arr={arr}')
             break
         else:
             i += 1
     
-        
+    # Hay que saber:
+    #   Qué porción del arreglo realmente queremos modificar
+    #   Comprobar que estamos modificando esa porción
+    #   Realizar el corrimiento
+    
+    if s>0:
+        for j in range(len(arr)-end_p, s-1, -1):
+            arr[j] = str(arr[j-s])
+        arr[s-1] = '0'
+        #arr[s] = 0
+        end_p = len(arr)
 
-    for j in range(len(arr)-end_p, s-1, -1):
-        arr[j] = arr[j-s]
-    arr[s-1] = 0
-    end_p = len(arr) - s -1
+
+    # Original
+    # for j in range(len(arr)-end_p, s-1, -1):
+    #     arr[j] = arr[j-s]
+    # arr[s-1] = 0
+    # arr[s] = 0
+    # end_p = len(arr) - s -1
 
     print(f's={s} \t arr={arr}')
     return arr, end_p
@@ -31,5 +44,8 @@ def duplicateZeros(arr, s=0, end_p = 1):
 
 
 if __name__=="__main__":
-    arri = [0,4,0,6,3,0,2,5,4,6,7]
-    print(f'\nresultado: {duplicateZeros(arri)}')
+    arr1 = [0,4,0,6,3,0,2,5,4,6,7]
+    arr2 = [1,2,3,0,4,5,6]
+    arr3 = [1,2,3,0,4,5,6,0,7,8,9]
+
+    print(f'\nresultado: {duplicateZeros(arr3)}')
