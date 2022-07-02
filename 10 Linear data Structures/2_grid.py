e264083c7f142a4b94ef8f17831e0c2f7fa877bc
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-07-02 14:21:28 -0500

    advanced a little

---
+++ 10 Linear data Structures/2_grid.py
@@ -0,0 +1,30 @@
+from .array import Array
+
+class Grid():
+    def __init__(self, rows, columns, fill_value=None) -> None:
+        self.data = Array(rows)
+        for row in range(rows):
+            self.data[row] = Array(columns, fill_value)
+
+    def get_hight(self):
+        return len(self.data)
+
+    def get_width(self):
+        return len(self.data[0])
+
+    def __get_item__(self, index):
+        return self.data[index]
+    
+    def __str__(self):
+        result = ""
+
+        for row in range(self.get_hight()):
+            for col in range(self.get_width()):
+                result += str(self.data[row][col]) + " "
+            result += '\n'
+
+        return str(result)
+
+if __name__=="__main__":
+    matrix = Grid(3,3)
+    print(matrix)
---
+++ 10 Linear data Structures/array.py
@@ -0,0 +1,49 @@
+"""Created our own array class
+"""
+
+class Array():
+    def __init__(self, capacity, fill_value=None):
+        self.items = list()
+        for i in range(capacity):
+            self.items.append(fill_value)
+        
+    def __len__(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+    def __iter__(self):
+        return iter(self.items)
+    
+    def __getIntem__(self, index):
+        return self.items[index]
+
+    def __setIntem__(self, index, newIntem):
+        self.items[index] = newIntem
+
+    def __sum__(self):
+        for i in range(self.__len__()):
+            if i == 0:  sum  = self.items[i]
+            else:       sum += self.items[i]
+        return sum
+        
+
+if __name__ == "__main__":
+    menu = Array(5)
+    print(menu.__len__())
+    print(menu.__str__())
+    print(menu.__iter__())
+    menu.__setIntem__(0, 0)
+    menu.__setIntem__(1, 2)
+    menu.__setIntem__(2, 4)
+    menu.__setIntem__(3, 8)
+    menu.__setIntem__(4, 16)
+    print(menu.__getIntem__(2))
+    print(menu.__sum__())
+    menu.__setIntem__(0, 'b')
+    menu.__setIntem__(1, 'o')
+    menu.__setIntem__(2, 'n')
+    menu.__setIntem__(3, 'i')
+    menu.__setIntem__(4, ' bb')
+    print(menu.__sum__())
