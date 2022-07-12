6caa9aa9ccc1459bbaa3bffe9aabbf9f6e9be066
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-07-12 13:50:43 -0500

    finished with es9 and learned regex

--- 13 ECMAScript 6+/src/es9/index.js
+++ 13 ECMAScript 6+/src/es9/index.js
@@ -39,3 +39,15 @@ helloWorld()
     .then(response => console.log(`Response is: ${response}`))
     .catch(error => console.log(`Error is: ${error}`))
     .finally(() => console.log("It has finished"));     // needs to be function
+
+
+// REGEX (regular expresions)
+//      to make match some regular expresions
+const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/;
+const match = regexData.exec('2018-04-20');
+const year = match[1];
+const month = match[2];
+const day = match[3];
+
+console.log(year, month, day);
+console.log(match)
