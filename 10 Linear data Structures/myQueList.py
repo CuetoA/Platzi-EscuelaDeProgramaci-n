65548a11f1b75a66586c6efd1c127d7621ec6364
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-07-20 00:25:10 -0500

    created my first que

--- 10 Linear data Structures/myQueList.py
+++ 10 Linear data Structures/myQueList.py
@@ -1,14 +1,38 @@
 class MyQue:
     def __init__(self) -> None:
         self.items = []
-        self.size = len(self.items)
+        self.size  = len(self.items)
         pass
 
     def enqueue(self, data):
         self.items.append(data)
+        self.size += 1
         
     def dequeue(self):
-        self.items.pop(0)
+        poped = self.items.pop(0)
+        self.size -= 1
+        return poped
 
-    def transverse():
-        
+    def transverse(self):
+        total_items = self.size
+        message = ""
+
+        for item in range(total_items):
+            # print(f"item is {item}")
+            message += str(self.items[item]) + ", "
+        print(message)
+
+
+if __name__ == "__main__":
+    food = MyQue()
+    food.enqueue("eggs")
+    food.enqueue("hotdog")
+    food.enqueue("tamarindo")
+    food.enqueue("wings")
+    food.enqueue("milk")
+    food.enqueue("jam")
+    food.transverse()
+
+    print(food.dequeue())
+    print(food.dequeue())
+    food.transverse()
