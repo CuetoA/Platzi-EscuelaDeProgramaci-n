5a1226949d7428bbef180bdc14d156f7b2107dd1
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-07-21 16:43:37 -0500

    learned to use angular

--- 14 Javascript Frameworks/2_Angular/my-app/src/app/app.component.ts
+++ 14 Javascript Frameworks/2_Angular/my-app/src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
 
+type Movie = {
+  name: String,
+  available: number,
+  quantity: number,
+};
+type Movies = Array<Movie>;
+
 @Component({
   selector: 'app-root',
+  // template: `<h2>Hey!</h2>`
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title: String = 'Movies';
+  movies: Movies = [
+  {
+      name: "Avengers",
+      available: 5,
+      quantity: 0,
+  },
+  {
+    name: "Forest Gump",
+    available: 2,
+    quantity: 0,
+  },
+  ];
+
+  addMovieQuantity (movieName: String){
+    const movieIndex: number = this.movies.findIndex(
+      movie => movie.name === movieName,
+    );
+
+      this.movies[movieIndex].quantity += 1;
+  }
+
+  rmvMovieQuantity (movieName: String){
+    const movieIndex: number = this.movies.findIndex(
+      movie => movie.name === movieName,
+    );
+
+      this.movies[movieIndex].quantity -= 1;
+  }
 }
