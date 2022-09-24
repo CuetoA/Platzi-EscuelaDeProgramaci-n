f7e17842dd970548726a5ccb8324c5c0ed6a6447
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-09-23 20:19:55 -0500

    practiced by myself and learning positioners

--- 9 Frontend development/002_practice.js
+++ 9 Frontend development/002_practice.js
@@ -1,20 +1,25 @@
 function button_click(){
     let height = document.getElementById('i_height').value;
     let character = document.getElementById('i_character').value;
+    let my_text_el = document.getElementById('my_text');
     
     // console.log(`height: ${height}`);
     // console.log(`character: ${character}`);
 
-    calculate_arr(height, character);
+    let my_str = calculate_arr(height, character);
+    // my_text_el.innerHTML = my_str;
 }
 
 let calculate_arr = (height, character) => {
-
+    
+    var my_str = ''
     for(let iter = 0; iter < height; iter ++){
         num_spaces = (height - iter) -1 ;
-        num_char = (iter * 2) +1;
+        num_char = (iter * 2) + 1
 
-        let my_str = ` `.repeat(num_spaces) + `${character}`.repeat(num_char);
-        console.log(my_str)
+        my_str += ` `.repeat(num_spaces) + `${character}`.repeat(num_char) + '\n';
     }
+
+    console.log(my_str);
+    return my_str
 }
