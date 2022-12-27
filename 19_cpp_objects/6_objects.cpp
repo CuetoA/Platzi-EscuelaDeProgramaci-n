998c0390e36f85a461d650fa59283d6754cf6aae
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2022-12-27 14:10:15 -0500

    creating an object

---
+++ 19_cpp_objects/6_objects.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+
+class Persona{
+public:
+    string name = "Andrés";
+    int age = 24;
+
+    void heloThere(string name){
+        cout << "hello there " << name << endl;
+    }
+};
+
+
+int main() {
+    
+    Persona* person = new Persona();
+    cout << person->name << endl;
+    person -> heloThere("Test");
+}
