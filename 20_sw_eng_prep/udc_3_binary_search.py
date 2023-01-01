2464a053e114b2ba0125a08135db22dedd1428a2
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2023-01-01 12:17:29 -0500

    cleaned code and wrote imporvements

--- 20_sw_eng_prep/udc_3_binary_search.py
+++ 20_sw_eng_prep/udc_3_binary_search.py
@@ -14,25 +14,22 @@ resructions:
 - non repited
 - positive and negative
 - no range
+
+improvements:
+- Better debbug methods
+- Analyze start and end point parameters
+- Clearer step by step analysis
 """
 
 def binary_search(input_array, value):
-    """Your code goes here."""
-
-    start = 0
-    end = len(input_array)
-    l   = len(input_array)
+    start   = 0
+    end     = len(input_array)
 
     while True:
         i = start + (end - start)//2
-        print(f"i is {i}")
 
-        # if i+1 < start  or  i >= end:
-        if  start > end:
-            return -1
-
-        if input_array[i] == value:
-            return i
+        if  start > end:                return -1
+        if input_array[i] == value:     return i
 
         bigger = value > input_array[i]
         if bigger:  start   = i + 1
