262918b971b16236210dd110dab0f2c2089b6e33
Andres Cueto <49458808+CuetoA@users.noreply.github.com>
2023-01-23 13:20:59 -0500

    added mode to development

--- 21_react/src/index.js
+++ 21_react/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ReactDOM } from "react";
+import ReactDOM from "react";
 import App from './components/app';
 
-ReactDOM.render(<App/>, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById("App"));
+console.log("im inside index.js jeje");
